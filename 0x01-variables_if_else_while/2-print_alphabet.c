87d7405a4b5a2a996aea40b72c3ad83814193cb6
Your Name <mshkatso.sul0@gmail.com>
2023-02-17 18:00:55 -0800

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Entry poit
+ *
+ * Return: Always= o (success)
+ */
+
+int main(void)
+{
+	char x;
+	for (x = 'a' ; x <= 'z'; x++)
+	{	putchar(x);
+	putchar('\n');
+	}
+	return 0;
+}
