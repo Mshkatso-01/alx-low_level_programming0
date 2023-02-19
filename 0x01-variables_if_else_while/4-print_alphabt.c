aba5e050bad8ad0563d68d2f42c88ab5bf617371
Your Name <mshkatso.sul0@gmail.com>
2023-02-19 04:57:56 -0800

    9-print_comb.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+ * main - Entry point 
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	char t;
+	for(t = 'a' ; t <= 'z' ; t++)
+	{
+		putchar(t);
+		if(t == 'c' || t == 'y' || t == 'o')
+			continue;
+	}
+
+	putchar('\n');
+
+	return 0;
+
+
+
+
+
+
+
+}
