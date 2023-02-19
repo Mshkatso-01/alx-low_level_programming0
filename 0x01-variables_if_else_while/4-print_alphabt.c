96771d84997b512b98844d89105ad7e939b06a95
Your Name <mshkatso.sul0@gmail.com>
2023-02-19 11:36:33 -0800

    4-print_alphabt.c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,22 +1,24 @@
 #include <stdio.h>
 /**
- * main - Entry point 
+ * main - Entry point
  *
  * Return: Always 0 (success)
  */
 int main(void)
+
 {
 	char t;
-	for(t = 'a' ; t <= 'z' ; t++)
+
+	for (t = 'a' ; t <= 'z' ; t++)
 	{
 		putchar(t);
-		if(t == 'c' || t == 'y' || t == 'o')
+		if (t == 'c' || t == 'y' || t == 'o')
 			continue;
 	}
 
 	putchar('\n');
 
-	return 0;
+	return (0);
 
 
 
