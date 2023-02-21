6a283869273083209015cc5373fc12d82334743e
Your Name <mshkatso.sul0@gmail.com>
2023-02-21 08:15:34 -0800

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,13 +1,14 @@
 #include "main.h"
 /**
- * maiin - print "_putchar, followed by a new line.
- * Description: not alllowed to include standard libraries.
+ * main - print "_putchar, followed by a new line.
+ * Description: you are  not alllowed to include standard libraries.
  * Return: 0.
  */
 int main(void)
 {
 	char xn[] = "_putchar";
 	int y = 0;
+
 	while (xn[y] != '\0')
 	{
 		_putchar(xn[y]);
