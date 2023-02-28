fa7c83d922b71037c636e469bd254ca3a6b876c2
Mishkat <mshkatso.sul0@gmail.com>
2023-02-28 10:19:45 -0800

    comply with betty style

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -11,18 +11,18 @@ void puts_half(char *string)
 	int odd_length;
 	int index;
 
-	while(string[length])
+	while (string[length])
 	{
 		length += 1;
 	}
 	odd_length = (length % 2 != 0);
 	index = length;
-	if(odd_length)
+	if (odd_length)
 	{
 		index += 1;
 	}
 	index /= 2;
-	while(string[index])
+	while (string[index])
 	{
 		_putchar(string[index]);
 		index += 1;
