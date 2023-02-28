ffcd3dfe953fb3677d51502286781ef59afa11b6
Mishkat <mshkatso.sul0@gmail.com>
2023-02-28 07:43:15 -0800

    add betty documentation

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,3 +1,9 @@
+/**
+ *swap_int - swap two integers
+ *@a: Pointer to the integer to be swapped with a
+ *@b: Pointer to the integer to be swapped with a
+ *Return: void
+ */
 void swap_int(int* A, int* B)
 {
 	int Temp = *A;
