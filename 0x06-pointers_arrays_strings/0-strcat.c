9766e30ed2e892d90eda1a4cdaea1a1277007461
Mishkat <mshkatso.sul0@gmail.com>
2023-03-01 01:59:00 -0800

    add 0-strcat.c

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,23 @@
+/**
+ *_strcat - concatenates two strings into the destination
+ *@dest: destination string
+ *@src: source string
+ *Return: char* - destination string
+ */
+char* _strcat(char* dest, char* src)
+{
+	int index1 = 0;
+	int index2 = 0;
+	while (dest[index1])
+	{
+		index1 += 1;
+	}
+	do
+	{
+		dest[index1] = src[index2];
+		index1 += 1;
+		index2 += 1;
+	} while (src[index2]);
+	return (dest);
+}
+
