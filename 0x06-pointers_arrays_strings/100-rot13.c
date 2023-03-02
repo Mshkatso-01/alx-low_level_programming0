536a829435684da3bab914d651ed273c504176c2
Mishkat <mshkatso.sul0@gmail.com>
2023-03-02 12:57:08 -0800

    comply with betty style

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -5,25 +5,25 @@
  */
 char *rot13(char *string)
 {
-  int i = 0;
-  int j = 0;
-  char data[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
-  char rotated[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+	int i = 0;
+	int j = 0;
+	char data[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char rotated[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
 
-  while (string[i])
-  {
-    while (data[j])
-    {
-      if (string[i] == data[j])
-      {
-        string[i] = rotated[j];
-        break;
-      }
-      j += 1;
-    }
-    j = 0;
-    i += 1;
-  }
-  return (string);
+	while (string[i])
+	{
+		while (data[j])
+		{
+			if (string[i] == data[j])
+			{
+				string[i] = rotated[j];
+				break;
+			}
+			j += 1;
+		}
+		j = 0;
+		i += 1;
+	}
+	return (string);
 }
 
