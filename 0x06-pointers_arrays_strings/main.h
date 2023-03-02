de482303aa4dc2e745cab6a6eaeec8365dda8a38
Mishkat <mshkatso.sul0@gmail.com>
2023-03-02 14:12:55 -0800

    modify main.h

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -5,10 +5,10 @@ char *_strncat(char *dest, char *src, int n);
 char *_strncpy(char *dest, char *src, int n);
 int _strcmp(char *s1, char *s2);
 void reverse_array(int *a, int n);
-char *string_toupper(char *);
-char *cap_string(char *);
-char *leet(char *);
-char *rot13(char *);
+char *string_toupper(char *string);
+char *cap_string(char *string);
+char *leet(char *string);
+char *rot13(char *string);
 void print_number(int n);
 void _putchar(int ch);
 #endif
